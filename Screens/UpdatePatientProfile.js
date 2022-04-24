7a5ea0e5df6171e08cea58f4c5db7b308b50d972
shafay-azeem <shafayazeem12@gmail.com>
2022-04-24 16:56:31 +0500

    updated

--- Screens/UpdatePatientProfile.js
+++ Screens/UpdatePatientProfile.js
@@ -1,7 +1,8 @@
-import {SafeAreaView,ScrollView, Text,StatusBar, View, TouchableOpacity,TextInput} from 'react-native';
-import React, {Component,Fragment} from 'react';
+import {SafeAreaView,ScrollView, Text,StatusBar, View, TouchableOpacity,TextInput,Alert} from 'react-native';
+import React, {Component,Fragment,useState} from 'react';
 import RadioForm  from 'react-native-simple-radio-button';
 import { Picker } from "@react-native-picker/picker";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import CheckBox from 'react-native-check-box';
 
@@ -14,40 +15,185 @@ import Header from './Header';
 import UnitClerkHeader from './AllHeaders/UnitClerkHeader';
 
 
-var radio_props_sv = [
-    {label: 'Clinical Visit', value: 1 },
-    {label: 'Educational Visit', value: 0 },
-    {label: 'Others', value: 2 }
-  ];
+
+
+
+  import { useNavigation } from '@react-navigation/native';
+
+ 
+  const UpdatePatientProfile = () => {
+
+
+
+  const navigation = useNavigation();  
+    let [FirstName, setFirstName] = useState('');
+    let [MiddleName, setMiddleName] = useState('');
+    let [LastName, setLastName] = useState('');
+    let [Age, setAge] = useState('');
+    let [MaritalStatus, setMaritalStatus] = useState('Unmarried');
+    let [Phone_Number, setPhone_Number] = useState('');
+    let[HomeAddress,setHomeAddress] = useState('')
+    let[password,setpassword] = useState('')
+    let[Email,setEmail] = useState('')
+    let[confirmpassword,setconfirmpassword] = useState('')
+    
+    let[gender,setgender] = useState('Male')
+    let [date, setDate] = useState('');
+    let [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  
+    const showDatePicker = () => {
+      setDatePickerVisibility(true);
+    };
+  
+    const hideDatePicker = () => {
+      setDatePickerVisibility(false);
+    };
+  
+    const handleConfirm = (date) => {
+      setDate(date);
+      hideDatePicker();
+    };
+  
+    const getDate = () => {
+      let tempDate = date.toString().split(' ');
+      return date !== ''
+        ? `${tempDate[0]} ${tempDate[1]} ${tempDate[2]} ${tempDate[3]}`
+        : '';
+    };
+
+
+
+    let register_user = () => {
+
+    
+
+  
+      if (!FirstName) {
+        alert('Please fill First Name');
+        return;
+      }
+
+      if (FirstName.trim().length < 3) {
+        alert('First Name must be minimum 3 characters');
+         return;
+        }
+
+      let regname=/^[A-Za-z]+(([,.] |[ '-])[A-Za-z]+)*([.,'-]?)$/;
+      if (regname.test(FirstName.toString())){
+      }
+      else{
+        alert('First Name should include only Alphabets');
+        return;
+      }
+
+      if (!MiddleName) {
+        alert('Please fill Middle Name');
+        return;
+      }
+      if (MiddleName.trim().length < 3) {
+        alert('Middle Name must be minimum 3 characters');
+         return;
+       }
+       if (regname.test(MiddleName.toString())){
+      }
+      else{
+        alert('Middle Name should include only Alphabets');
+        return;
+      }
+      if (!LastName) {
+        alert('Please fill Last Name');
+        return;
+      }
+      if (LastName.trim().length < 3) {
+        alert('Last Name must be minimum 3 characters');
+         return;
+       }
+
+       if (regname.test(LastName.toString())){
+       }
+      else{
+        alert('Last Name should include only Alphabets');
+        return;
+      }
+      if (!Phone_Number) {
+        alert('Please Enter Phone Number');
+        return;
+      }
+
+      if (Phone_Number.trim().length < 12 ||Phone_Number.trim().length > 12 ) {
+        alert('Phone Number must be of 11 digit');
+         return;
+       }
+         
+      let regNum=/^[0-9]{4}[-]{1}[0-9]{7}$/;
+      if (regNum.test(Phone_Number.toString())){
+
+      }
+      else{
+        alert('Please Enter Number Acc to our Hint')
+        return;
+      }
+
+      
+      if (!Email) {
+        alert('Please fill Email');
+        return;
+      }
+
+      let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+      if (reg.test(Email) === false) {
+      alert('enter valid email address');
+      return;
+      }
+  
+
+    
+      if (!Age) {
+        alert('Please fill Address');
+        return;
+      }
+
+       let regAge=/^100|[1-9]?\d$/
+       if (regAge.test(Age.toString())){
+
+       }
+       else{
+         alert('Enter Valid Age')
+         return;
+       }
+       if (!password) {
+        alert('Please Enter Password');
+        return;
+      }
+      if (!confirmpassword) {
+        alert('Please Enter Password');
+        return;
+      }
+      if(password!=confirmpassword){
+        alert('password must be match');
+        return;
+      }
+
+     
+
+            if (password!=null) {
+              Alert.alert(
+                'Success',
+                'Updated Successfully',
+                [
+                  {
+                    text: 'Ok',
+                    onPress: () => navigation.navigate('PatientDemographics'),
+                  },
+                ],
+                { cancelable: false }
+              );
+            } else alert('Failed');
+          }
+   
   
 
 
-export default class UpdatePatientProfile extends Component {
-    constructor(props){
-        super(props)
-    
-       }
-        state={
-          FirstName:"",
-          MiddleName:"",
-          LastName:"",
-            Gender:"",
-            Age:'',
-            DOB:"",
-            MaritalStatus:'',
-            phoneNumber:"",
-            address:'',
-            Email:"",
-            password:"",
-            confirmpassword:"",
-
-    
-      }
-     
-
-   render(){
-
- 
 
     return (
       <View style={styles.container} >
@@ -70,7 +216,7 @@ export default class UpdatePatientProfile extends Component {
           <TextInput style={styles.Edittext}
           placeholder="Enter First Name" 
           placeholderTextColor="#3FB39B"
-           onChangeText={text => this.setState({FirstName:text})}/>
+          onChangeText={(FirstName) => setFirstName(FirstName)}/>
         </View>
 
         <View style={styles.inputWrap}>
@@ -78,7 +224,7 @@ export default class UpdatePatientProfile extends Component {
           <TextInput style={styles.Edittext}
            placeholder="Enter Middle Name" 
            placeholderTextColor="#3FB39B"
-            onChangeText={text => this.setState({MiddleName:text})} />
+           onChangeText={ (MiddleName) => setMiddleName(MiddleName)}/>
         </View>
 
         <View style={styles.inputWrap}>
@@ -86,7 +232,7 @@ export default class UpdatePatientProfile extends Component {
           <TextInput style={styles.Edittext}
            placeholder="Enter Last Name" 
            placeholderTextColor="#3FB39B"
-            onChangeText={text => this.setState({LastName:text})} />
+           onChangeText={ (LastName) => setLastName(LastName)}/>
         </View>
       </View>
     
@@ -99,9 +245,9 @@ export default class UpdatePatientProfile extends Component {
         backgroundColor:'#F7F7F7',     
            borderWidth: 1,  borderRadius: 15,  marginHorizontal:20,   marginTop:2, height:50}}> 
           <Picker  
-           selectedValue={this.state.PickerSelectedVal}
+           selectedValue={gender}
            placeholderTextColor="#3FB39B"
-           onValueChange={(itemValue, itemIndex) => this.setState({PickerSelectedVal: itemValue})} >
+           onValueChange={(itemValue, itemIndex) => setgender(itemValue)} >
   
            <Picker.Item color='#3FB39B' label="Male" value="Male" />
            <Picker.Item color='#3FB39B' label="Female" value="Female" />
@@ -122,17 +268,32 @@ export default class UpdatePatientProfile extends Component {
            placeholder="Enter Age" 
            keyboardType = 'numeric'
            placeholderTextColor="#3FB39B"
-            onChangeText={text => this.setState({Age:text})} />
+           onChangeText={ (Age) => setAge(Age)}/>
         </View>
 
         <View style={styles.inputWrap}>
-          <Text style={styles.EdittextHeading}>DOB</Text>
+        <TouchableOpacity  onPress={showDatePicker}>
+             <Text style={styles.EdittextHeading}>DOB</Text>
+             <View>
+               
+             <TextInput
+            style={styles.Edittext}
+            value={getDate()}
+            editable={false} 
+            selectTextOnFocus={false}
+            color="#000000"
+            placeholderTextColor="#30A28C"
+            placeholder="Date"/>
 
-          <Example/>
-          {/* <TextInput style={styles.Edittext}
-           placeholder="Enter DOB" 
-           placeholderTextColor="#3FB39B"
-            onChangeText={text => this.setState({DOB:text})} /> */}
+<DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+      />
+     
+              </View>
+              </TouchableOpacity>
         </View>
       </View>
 
@@ -142,10 +303,10 @@ export default class UpdatePatientProfile extends Component {
       <View style={styles.inputWrap}>
       <Text style={styles.EdittextHeading}>phoneNumber</Text>
           <TextInput style={styles.Edittext}
-           placeholder="Enter phoneNumber" 
+            placeholder="03XX-XXXXXXX" 
            keyboardType = 'numeric'
            placeholderTextColor="#3FB39B"
-            onChangeText={text => this.setState({phoneNumber:text})} />
+           onChangeText={ (Phone_Number) => setPhone_Number(Phone_Number)}/>
         </View>
 
 
@@ -155,18 +316,21 @@ export default class UpdatePatientProfile extends Component {
 <View   style={{   borderColor: "#3FB39B",
         backgroundColor:'#F7F7F7',     
            borderWidth: 1,  borderRadius: 15,  marginHorizontal:20,   marginTop:2, height:50}}> 
-          <Picker  
-           selectedValue={this.state.PickerSelectedVal}
-           placeholderTextColor="#3FB39B"
-           onValueChange={(itemValue, itemIndex) => this.setState({PickerSelectedVal: itemValue})} >
+           <Picker  
+           selectedValue={MaritalStatus}
+           placeholderTextColor="#30A28C"
+            onValueChange={(itemValue, itemIndex) => setMaritalStatus(itemValue)} 
+            
+            >
   
-           <Picker.Item color='#3FB39B' label="Married" value="Married" />
-           <Picker.Item color='#3FB39B' label="Unmarried" value="Unmarried" />
-           <Picker.Item color='#3FB39B' label="Divorced" value="Divorced" />
-           <Picker.Item  color='#3FB39B' label="Widow" value="Widow" />
+           <Picker.Item color='#30A28C' label="Married" value="Married" />
+           <Picker.Item color='#30A28C' label="Unmarried" value="Unmarried" />
+           <Picker.Item color='#30A28C' label="Divorced" value="Divorced" />
+           <Picker.Item  color='#30A28C' label="Widow" value="Widow" />
         
 
-           </Picker> 
+           </Picker>  
+
 
        
      
@@ -180,7 +344,7 @@ export default class UpdatePatientProfile extends Component {
               style={styles.Edittext}
               placeholder="Enter your Address" 
               placeholderTextColor="#3FB39B"
-              onChangeText={text => this.setState({address:text})}/>
+              onChangeText={ (HomeAddress) => setHomeAddress(HomeAddress)}/>
          
          
 
@@ -192,7 +356,7 @@ export default class UpdatePatientProfile extends Component {
           <TextInput style={styles.Edittext}
           placeholder="Enter Email" 
           placeholderTextColor="#3FB39B"
-           onChangeText={text => this.setState({Email:text})}/>
+          onChangeText={ (Email) => setEmail(Email)}/>
         </View>
 
         <View style={styles.inputWrap}>
@@ -201,7 +365,7 @@ export default class UpdatePatientProfile extends Component {
             secureTextEntry={true}
            placeholder="Enter Password" 
            placeholderTextColor="#3FB39B"
-            onChangeText={text => this.setState({password:text})} />
+           onChangeText={ (password) => setpassword(password)}/>
         </View>
 
         <View style={styles.inputWrap}>
@@ -211,7 +375,7 @@ export default class UpdatePatientProfile extends Component {
            placeholder="Enter Password" 
            placeholderTextColor="#3FB39B"
         
-            onChangeText={text => this.setState({confirmpassword:text})} />
+           onChangeText={ (confirmpassword) => setconfirmpassword(confirmpassword)}/>
         </View>
       </View>
 
@@ -219,7 +383,7 @@ export default class UpdatePatientProfile extends Component {
 <View style={{flexDirection:'row',marginTop:30}}>
 
   <View style={{width:"50%",flexDirection:'row',alignSelf:'center',alignItems:'center',marginLeft:27}}>
-             <CheckBox
+             {/* <CheckBox
 
       
          
@@ -233,7 +397,7 @@ isChecked={this.state.isChecked}
 RightText={"Remember Me"}
 checkedColor={"#3FB39B"}
 
-/>
+/> */}
 
              <Text style= {[{fontFamily:"Montserrat-Bold",justifyContent:'center',color:"#3FB39B",fontSize:15}]}>Please Verify above Information is Correct</Text>
              </View>
@@ -241,7 +405,8 @@ checkedColor={"#3FB39B"}
 
 
 <TouchableOpacity style={[styles.buttonForm]}
-   onPress={() =>this.props.navigation.navigate("PatientDemographics")}
+  //  onPress={() =>navigation.navigate("PatientDemographics")}
+  onPress={register_user}
    > 
    <Text style={styles.Button_text_styling}>
    UPDATE </Text>
@@ -265,7 +430,9 @@ checkedColor={"#3FB39B"}
       </View>
       
     );
-   }
-}
+   };
+
+
+  export default UpdatePatientProfile;
 
 
